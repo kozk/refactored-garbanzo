0bffc086b7b7e254b96a0ce4649ad18bc9ffcfc7
kozk <itzcui@gmail.com>
2017-05-19 21:37:01 -0500

    update p1,sh

--- p1/p1.sh
+++ p1/p1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "INFORMACION DE LOS CORES Y MEMORIA EN TIEMPO REAL"
-echo "TOTAL DE CORES: `top -i | head -3 | grep %Cpu | awk '{print $2,$4}' | paste -sd + `"
+echo "TOTAL DE CORES: `top -i | grep %Cpu | awk '{print $2"\n"$4}' | paste -sd + | bc`"
 echo "TOTAL DE MEMORIA: `awk '/Mem:/ {print $2}' <(free -gh)`"
