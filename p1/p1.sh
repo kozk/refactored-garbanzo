3ee0550167da4905497641783d7c1a230dce93e5
kozk <itzcui@gmail.com>
2017-05-20 02:11:13 -0500

    update p5.sh

--- p1/p1.sh
+++ p1/p1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "INFORMACION DE LOS CORES Y MEMORIA EN TIEMPO REAL"
 echo "USO TOTAL DE CPU: `top -i | head -3 | grep %Cpu | awk '{print $2"\n"$4}' | paste -sd + | bc`"
-echo "USO TOTAL DE MEMORIA: `top -i | head -4 | grep Mem | awk '{print $6}'`"
+echo "USO TOTAL DE MEMORIA EN KB: `top -i | head -4 | grep Mem | awk '{print $6}'`"
