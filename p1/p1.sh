f1937d60086cf3037e9e12ff454428e88ac79d72
Gabriel <cbi205322069@titlani.uam.mx>
2017-05-19 21:19:18 -0500

    Update p1.sh

--- p1/p1.sh
+++ p1/p1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "INFORMACION DE LOS CORES Y MEMORIA EN TIEMPO REAL"
-echo "TOTAL DE CPU: `top -i | head -3 | grep %Cpu | awk '{print $2,$4}' | paste -sd + `"
+echo "INFORMACION DE CPU Y MEMORIA EN TIEMPO REAL"
+echo "TOTAL DE CPU: `top -i | head -3 | grep %Cpu | awk '{print $2 \n$4}' | paste -sd + `"
 echo "TOTAL DE MEMORIA: `awk '/Mem:/ {print $2}' <(free -gh)`"
