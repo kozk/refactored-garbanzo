cc29607de38e459176cfa5f040227f3dbbdcdfef
Gabriel <cbi205322069@titlani.uam.mx>
2017-05-22 20:33:23 -0500

    Update p1.sh

--- p1/p1.sh
+++ p1/p1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "SISTEMAS OPERATIVOS - PRACTICA 1 - PROBLEMA 1"
-echo "INFORMACION DE LOS CORES Y CPU EN TIEMPO REAL"
+echo "INFORMACION DE CPU Y MEMORIA EN TIEMPO REAL"
 echo "USO TOTAL DE CPU: `top -i | head -3 | grep %Cpu | awk '{print $2"\n"$4}' | paste -sd + | bc`"
 echo "USO TOTAL DE MEMORIA EN KB: `top -i | head -4 | grep Mem | awk '{print $6}'`"
