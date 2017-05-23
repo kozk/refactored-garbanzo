347591b061b5df5e4f6c7ef09021cc9cd90940aa
Gabriel <cbi205322069@titlani.uam.mx>
2017-05-23 05:37:52 -0500

    Delete p5.sh

--- p1/p5.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "SISTEMAS OPERATIVOS - PRACTICA 1 - PROBLEMA 5"
-echo "TOTAL DE PROCESOS EN EL SISTEMA: `top -i | head -2 | grep Tasks | awk '{print $2}'`"
-echo "TOTAL DE MEMORIA QUE CONSUMEN: `top -i | head -4 | grep Mem | awk '{print $6}'`"
